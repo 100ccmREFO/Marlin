d17fde16a127af075ceb4bf9738cfd2b64db6b8d
100ccmREFO <49035879+100ccmREFO@users.noreply.github.com>
2020-11-30 14:55:44 +0100

    LinAdvance & Junction deviation
    Noch nicht perfekt

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -807,7 +807,7 @@
  *   https://blog.kyneticcnc.com/2018/10/computing-junction-deviation-for-marlin.html
  */
 #if DISABLED(CLASSIC_JERK)
-  #define JUNCTION_DEVIATION_MM 0.013 // (mm) Distance from real junction edge
+  #define JUNCTION_DEVIATION_MM 0.06 // (mm) Distance from real junction edge
   #define JD_HANDLE_SMALL_SEGMENTS    // Use curvature estimation instead of just the junction angle
                                       // for small segments (< 1mm) with large junction angles (> 135°).
 #endif
@@ -981,7 +981,7 @@
  *     |    [-]    |
  *     O-- FRONT --+
  */
-#define NOZZLE_TO_PROBE_OFFSET { -37.88, -6.5, -1.67 }
+#define NOZZLE_TO_PROBE_OFFSET { -37.88, -6.5, -1.60 }
 
 // Most probes should stay away from the edges of the bed, but
 // with NOZZLE_AS_PROBE this can be negative for a wider probing area.
