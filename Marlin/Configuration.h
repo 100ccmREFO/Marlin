005d670499203405bc7028eaea3aa1d3c8600398
100ccmREFO <49035879+100ccmREFO@users.noreply.github.com>
2020-12-01 17:46:55 +0100

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -70,7 +70,7 @@
 // @section info
 
 // Author info of this build printed to the host during boot and M115
-#define STRING_CONFIG_H_AUTHOR "(Joel Neuhaus 28.11.2020 SKR 1.4 Turbo)" // Who made the changes.
+#define STRING_CONFIG_H_AUTHOR "(Joel Neuhaus 01.12.2020 SKR 1.4 Turbo)" // Who made the changes.
 //#define CUSTOM_VERSION_FILE Version.h // Path from the root directory (no quotes)
 
 /**
