02167db6cc662e270ef4c466a335c777da505865
100ccmREFO <49035879+100ccmREFO@users.noreply.github.com>
2021-03-07 20:56:30 +0100

    Update Configuration.h

--- Marlin/Configuration.h
+++ Marlin/Configuration.h
@@ -1048,7 +1048,7 @@
 #define Z_PROBE_FEEDRATE_FAST (20*60)
 
 // Feedrate (mm/min) for the "accurate" probe of each point
-#define Z_PROBE_FEEDRATE_SLOW (Z_PROBE_FEEDRATE_FAST/ (5/2))
+#define Z_PROBE_FEEDRATE_SLOW (Z_PROBE_FEEDRATE_FAST/ 4)
 
 /**
  * Probe Activation Switch
